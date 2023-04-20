3f346157e051980c7737c3ba458411796c219347
Michal Korejwo <korejwo.michal@gmail.com>
2023-04-20 10:13:00 +0200

    move Chat to file

--- server.php
+++ server.php
@@ -1,73 +1,19 @@
 #!/usr/bin/env php
 <?php
 
-use Ratchet\ConnectionInterface;
 use Ratchet\Http\HttpServer;
-use Ratchet\MessageComponentInterface;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
 
 require __DIR__ . '/vendor/autoload.php';
-
-class MyChat implements MessageComponentInterface
-{
-    protected SplObjectStorage $clients;
-
-    public function __construct()
-    {
-        $this->clients = new SplObjectStorage;
-    }
-
-    /**
-     * @param ConnectionInterface $conn
-     * @return void
-     */
-    public function onOpen(ConnectionInterface $conn): void
-    {
-        $this->clients->attach($conn);
-    }
-
-    /**
-     * @param ConnectionInterface $from
-     * @param $msg
-     * @return void
-     */
-    public function onMessage(ConnectionInterface $from, $msg): void
-    {
-        foreach ($this->clients as $client) {
-//            if ($from != $client) {
-                $client->send($msg);
-//            }
-        }
-    }
-
-    /**
-     * @param ConnectionInterface $conn
-     * @return void
-     */
-    public function onClose(ConnectionInterface $conn): void
-    {
-        $this->clients->detach($conn);
-    }
-
-    /**
-     * @param ConnectionInterface $conn
-     * @param Exception $e
-     * @return void
-     */
-    public function onError(ConnectionInterface $conn, Exception $e): void
-    {
-        $conn->close();
-    }
-}
+require 'Socket/Chat.php';
 
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
-            new MyChat
+            new Chat
         )
     ),
-    8080,
-    '0.0.0.0'
+    8080
 );
 $server->run();
