51b82d9cbd28553baeaa6e7a9a0fa4b9d89d62c5
Michal Korejwo <korejwo.michal@gmail.com>
2023-04-20 10:32:49 +0200

    chat inside php

--- server.php
+++ server.php
@@ -1,12 +1,78 @@
 #!/usr/bin/env php
 <?php
 
+use Ratchet\ConnectionInterface;
 use Ratchet\Http\HttpServer;
+use Ratchet\MessageComponentInterface;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
 
 require __DIR__ . '/vendor/autoload.php';
-require 'Socket/Chat.php';
+
+class Chat implements MessageComponentInterface
+{
+    protected SplObjectStorage $clients;
+
+    public function __construct()
+    {
+        $this->clients = new SplObjectStorage;
+    }
+
+    /**
+     * @param ConnectionInterface $conn
+     * @return void
+     */
+    public function onOpen(ConnectionInterface $conn): void
+    {
+        // Store the new connection to send messages to later
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    /**
+     * @param ConnectionInterface $conn
+     * @return void
+     */
+    public function onClose(ConnectionInterface $conn): void
+    {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected.\n";
+    }
+
+    /**
+     * @param ConnectionInterface $conn
+     * @param Exception $e
+     * @return void
+     */
+    public function onError(ConnectionInterface $conn, Exception $e): void
+    {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
+
+    /**
+     * @param ConnectionInterface $from
+     * @param string $msg
+     * @return void
+     */
+    public function onMessage(ConnectionInterface $from, $msg): void
+    {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        foreach ($this->clients as $client) {
+//            if ($from !== $client) {
+            // The sender is not the receiver, send to each client connected
+            $client->send($msg);
+//            }
+        }
+    }
+}
 
 $server = IoServer::factory(
     new HttpServer(
